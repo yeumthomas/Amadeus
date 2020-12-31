2357be44884075c47472d3ed9ac15846bb1241a3
Thomas Yeum <yeum.thomas@gmail.com>
2020-12-31 04:56:17 -0600

    streamlined changes to script

--- algorithm/sync_video.py
+++ algorithm/sync_video.py
@@ -2,10 +2,16 @@ import ffmpeg
 import os
 from scipy.io.wavfile import read, write
 import matplotlib.pyplot as plt
-import numpy as numpy
-import cv2
+import numpy
+import math
 
-# helpful link to figure out os paths: https://stackoverflow.com/questions/18114081/ffmpeg-cannot-detect-file-while-os-can
+"""
+FINAL VIDEO DESCRIPTION
+- audio: 4410 sr
+- video: .mp4, fr: 25 fps
+- codec: h.264
+"""
+
 REL_ROOT = os.getcwd() # gets current working directory (ludwig-2.0 apparently)
 V_OUTPUT_PATH = REL_ROOT + '/algorithm/public/video_extracts/'
 A_OUTPUT_PATH = REL_ROOT + '/algorithm/public/audio_extracts/'
@@ -25,79 +31,106 @@ in1 = ffmpeg.input(REL_ROOT + '/algorithm/public/1.mov')
 in2 = ffmpeg.input(REL_ROOT + '/algorithm/public/2.mov')
 in3 = ffmpeg.input(REL_ROOT + '/algorithm/public/3.mov')
 in4 = ffmpeg.input(REL_ROOT + '/algorithm/public/4.mov')
-# claptest = ffmpeg.input(REL_ROOT + '/algorithm/public/claptest.mov').audio
 
 
-# ENTIRE HANDLER
-def editor(files):
+def editor(folder_path, work_num, output_file_name, output_file_type):
     """
-    handles all functions
+    Handles all functions.
+    Input:
+        - folder_path: path to folder housing all video files ordered in order to be shown in video.
+        - work_num: main number address for client
+        - output_file_name: name of final video file
+        - output_file_type: type of video file for final video
+    Output:
+        - path to final video
     """
+    ffmpeg_v_list = []
+    for file in os.scandir(folder_path):
+        ffmpeg_v_list.append(ffmpeg.input(file.path))
+    
+    # GENERATE FOLDER FOR WORKSPACE
+    workspace = generate_dir(os.getcwd() + '/algorithm/assets/', work_num)
 
-    merged_audio_path, cut_off_time = audio_handler([files[0].audio, files[1].audio, files[2].audio, files[3].audio])
+    # AUDIO
+    ffmpeg_a_list = [video.audio for video in ffmpeg_v_list]
+    merged_audio_path, cut_off_time = audio_handler(ffmpeg_a_list, workspace)
     audio = ffmpeg.input(merged_audio_path)
     
-    trimmed_vids = trim_vids(files, cut_off_time)
-
-    ### DETERMINE VIDEO LAYOUT HERE ###
-
-    compressed_vids = []
-    for video in trimmed_vids:
-        compressed_vids.append(compress_v(video, 2))
-
-    video = generate_mosaic([[compressed_vids[0], compressed_vids[1]], [compressed_vids[2], compressed_vids[3]]])
-
-    out = ffmpeg.output(video, audio, "COMPLETED.mp4", vcodec='h264', acodec='aac', strict='experimental')
+    # VIDEO
+    trimmed_vids = trim_vids(ffmpeg_v_list, cut_off_time)
+    video = generate_mosaic(trimmed_vids)
+    
+    # MERGE AV
+    finished_file_path = workspace + output_file_name + output_file_type
+    out = ffmpeg.output(video, audio, finished_file_path, vcodec='h264', acodec='aac', strict='experimental')
     out.run()
 
+    return finished_file_path
+
+def generate_dir(path, name):
+    """
+    creates new directory
+    Inputs:
+        - path: address to new directory
+        - name: name of directory
+    Outputs:
+        - path to new directory
+    """
+    dir_path = os.path.join(path, name)
+    os.mkdir(dir_path)
+    dir_path = dir_path + '/'
+    return dir_path
 
 # AUDIO OPERATIONS
-def audio_handler(alist):
+def audio_handler(alist, workspace):
     """
     Synchronizes and mixes all individual audio files into single audio file.
     Inputs:
         - alist: list of paths to audio files
+        - workspace: path to workspace folder
     Outputs:
         - path to single mixed audio file
         - time of cut-offs (to be sync'd with videos)
     """
 
     # creates and gets file paths for indvidiual wav files
-    file_path_list = extract_audio(alist)
+    file_path_list = extract_audio(alist, workspace)
 
     trimmed_path_list = []
     cut_off_time = []
+    audio_trimmed_path = generate_dir(workspace, "audio_trimmed")
     for ind in range(len(file_path_list)):
 
         # finds cut-off time on claps
         (cut_off, sample_rate, data) = find_clap(file_path_list[ind])
-        
+
         # creates and gets file paths for trimmed wav files
         delay = 1
-        trimmed_path_list.append(trim_audio(data, cut_off, sample_rate, ind, delay))
-
+        trimmed_path_list.append(trim_audio(data, cut_off, sample_rate, ind, audio_trimmed_path, delay))
         cut_off_time.append(cut_off / float(sample_rate) + delay)
 
     # merges all files into one
-    return sync_audio(trimmed_path_list), cut_off_time
+    return sync_audio(trimmed_path_list, workspace), cut_off_time
 
-def extract_audio(alist):
+def extract_audio(alist, workspace):
     """
     extracts audio from .audio ffmpeg streams into .WAV files
     (called by generate_mixed_audio)
     Inputs:
         - alist: list of .audio streams
+        - workspace: path to workspace folder
     Outputs:
         - wav_alist: list of paths to wav files in audio_extracts
     """
     wav_alist = []
     a_tracker = 0 # names files incrementing by 1
 
+    audio_extracts_path = generate_dir(workspace, "audio_extracts")
     for a in alist:
-        name = A_OUTPUT_PATH + str(a_tracker) + '.wav'
+        name = audio_extracts_path + str(a_tracker) + '.wav'
         a.output(name, audio_bitrate="160k").run()
         a_tracker += 1
-    for file in os.scandir(A_OUTPUT_PATH):
+    for file in os.scandir(audio_extracts_path):
         wav_alist.append(file.path)
 
     return wav_alist
@@ -123,7 +156,6 @@ def find_clap(afile):
         modified_data = list(map(abs, data[:CLAP_TIMER * sample_rate, 0]))
     
     # graph_audio(modified_data)
-
     threshold = max(modified_data) * THRESH_CAP # gets max loudness of data and sets 40% loudness as a clap
 
     print("")   
@@ -137,7 +169,7 @@ def find_clap(afile):
         
     print("ERROR FINDING CLAP")
 
-def trim_audio(afile, cut_off, sample_rate, index, delay=1):
+def trim_audio(afile, cut_off, sample_rate, index, audio_trimmed_path, delay=1):
     """
     trims audio 1 seconds after clap.
     Inputs:
@@ -145,25 +177,28 @@ def trim_audio(afile, cut_off, sample_rate, index, delay=1):
         - cut_off: location in seconds of clap
         - sample_rate: sample rate of audio file
         - index: index of audio file in main list of audio files
-        - number of seconds after cut_off to trim video
+        - audio_trimmed_path: file path to workspace directory
+        - delay: number of seconds after cut_off to trim video
     Return:
         - path to trimmed audio file
     """
+
     trimmed_clip = afile[(cut_off + sample_rate * delay):]
-    trimmed_file_name = A_TRIMMED_PATH + str(index) + ".wav"
+    trimmed_file_name = audio_trimmed_path + str(index) + ".wav"
     write(trimmed_file_name, sample_rate, trimmed_clip)
 
     return trimmed_file_name
 
-def sync_audio(trimmed_path_list):
+def sync_audio(trimmed_path_list, workspace):
     """
     synchronize all audio into a single wav file
     Inputs:
         - trimmed_path_list: paths to all trimmed audio files
+        - workspace: path to workspace directory
     Output:
         - path to final wav file
     """
-    merged_path = A_MERGED_PATH + 'merged.wav'
+    merged_path = workspace + 'merged.wav'
     a_inputs = [ffmpeg.input(path) for path in trimmed_path_list]
 
     while len(a_inputs) > 1:
@@ -172,6 +207,7 @@ def sync_audio(trimmed_path_list):
     
     merged_data = a_inputs[0]
     ffmpeg.output(merged_data, merged_path).run()
+
     return merged_path
 
 def graph_audio(data):
@@ -185,6 +221,7 @@ def graph_audio(data):
 
 
 
+# VIDEO OPERATIONS
 def trim_vids(vlist, cut_off_time):
     """
     trims videos to match trimmed audio
@@ -195,54 +232,111 @@ def trim_vids(vlist, cut_off_time):
         - trimmed video objects
     """
     trimmed_vlist = []
-
+    FPS_FINAL = 25
     for ind, video in enumerate(vlist):
-        cap=cv2.VideoCapture(video)
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        trimmed_vlist.append(ffmpeg.trim(video, start_frame=cut_off_time[ind] * fps).setpts('PTS-STARTPTS'))
+        stream = ffmpeg.filter(video, 'fps', fps=FPS_FINAL, round='up')
+        trimmed_vlist.append(ffmpeg.trim(stream, start_frame=cut_off_time[ind] * FPS_FINAL).setpts('PTS-STARTPTS'))
 
     return trimmed_vlist
 
 def generate_mosaic(vlist):
     """
-    Generates a r x c mosaic video output
+    Generates the mosaic-style for final video and compresses files accordingly.
     Inputs:
-        - vlist: list of lists where each of the x nested lists is a row with x videos
-    Returns:
-        - rxc mosaic workflow
+        - vlist: list of ffmpeg video objects
+    Output:
+        - path to final video
     """
+    def propose_mosaic(vlist):
+        """
+        creates a proposed mosaic for videos
+        Inputs:
+            - vlist: list of videos
+        Returns:
+            - proposed video format (ex: [4, 5, 5] for 14 videos, 3 rows, 5 videos each row except 1st)
+        """
+        vnum = len(vlist)
+        lower_square = math.floor(math.sqrt(vnum))
+        check = False
+
+        while not check:
+            mosaic_form = []
+            rows = lower_square
+            remainder = vnum % rows
+            cols = vnum // rows
+
+            for dummy in range(rows):
+                mosaic_form.append(cols)
+
+            print(mosaic_form)
+            if remainder:
+                rev_ind = len(mosaic_form) - 1
+                for dummy in range(remainder):
+                    mosaic_form[rev_ind] = mosaic_form[rev_ind] + 1
+                    rev_ind -= 1
+                    print(mosaic_form)
+            
+            check = check_dims(len(mosaic_form), mosaic_form[-1])
+            lower_square += 1
+
+        return mosaic_form
+
+    def check_dims(r, c):
+        """
+        Checks to see if width of videos are greater than height (we want horizontal videos in mosaic)
+        Inputs:
+            - r: # of rows in proposed mosaic
+            - c: # of cols in row with most columns in proposed mosaic
+        Outputs:   
+            - True, if mosaic passes this check
+            - False, if otherwise
+        """
+        if 1920 / c > 1080 / r:
+            return True
+        else:   
+            return False
+
+    def compress_v(video, rows, cols):
+        """
+        Compresses video to match how many rows, cols it contains
+        Inputs:
+            - video: video path
+            - rows: number of rows
+            - cols: number of cols for row video is in
+        Outputs:
+            - new video artifact with compression added to workflow
+        """
+        w, h = 1920, 1080
+        return ffmpeg.filter(video, 'scale', width=w/cols, height=h/rows)
+
+    mosaic_form = propose_mosaic(vlist)
+
+    # Compress each individual video by their dimensions in mosaic.
+    total_rows = len(mosaic_form)
+    mosaic_matrix = [[] for dummy in range(total_rows)]
+    curr_vid = 0
+    for row, total_cols in enumerate(mosaic_form, start=0):
+        for v_index in range(total_cols):
+            print("")
+            print("row: ", row)
+            print("v_index: ", v_index)
+            print("curr_vid: ", curr_vid)
+            print("total_rows: ", total_rows)
+            print("total_cols: ", total_cols)
+            print("")
+
+            mosaic_matrix[row].append(compress_v(vlist[curr_vid], total_rows, total_cols))
+            curr_vid += 1
+    
+    print("MOSAIC_MATRIX: ", mosaic_matrix)
+    # Prepare ffmpeg xstack.
     hstack_lst = []
-    for row in vlist:
+    for row in mosaic_matrix:
         hstack_lst.append(ffmpeg.filter(row, 'hstack', len(row)))
-    return ffmpeg.filter(hstack_lst, 'vstack', len(vlist))
+    return ffmpeg.filter(hstack_lst, 'vstack', len(hstack_lst))
 
-def compress_v(in_vid, num_r_c):
-    """
-    Compresses video to match how many rows, cols it contains
-    Inputs:
-        - in_vid: video object inputted
-        - num_r_c: integer representing number of both rows and columns of final vid (row = col here)
-    Return:
-        - new video artifact with compression added to workflow
-    """
-    w, h = 1920, 1080
-    res_dict = {
-        1: [w, h],
-        2: [w/2, h/2],
-        3: [w/3, h/3],
-        4: [w/4, h/4],
-        5: [w/5, h/5],
-        6: [w/6, h/6],
-        7: [w/7, h/7] # not currently sure if breaks since not perfectly divisible
-    }
-    return ffmpeg.filter(in_vid, 'scale', width=res_dict[num_r_c][0], height=res_dict[num_r_c][1]) # allows x.0 for input w, h
 
-# input22 = compress_v(input2, 2).split()
-# input2main = compress_v(main, 2).split()
-# input23 = compress_v(input3, 2).split()
-# generate_mosaic([[input22[0], input22[1]], [input23[0], input23[1]]]).output(aud, V_OUTPUT_PATH + "test_again_3.10.mp4").run()
-
-editor([in1, in2, in3, in4])
+editor(os.getcwd() + '/algorithm/assets/me_singing', "DEMO PROJECT", "acapella", ".mp4")
 
 """
 NOTES
@@ -302,6 +396,8 @@ QUESTIONS/GOALS FOR TMR
 - compress_v take in a list of video files rather than 1
 - trimming video is not exact, maybe fix decimal (find framerate of vid and go by frames)
 
+# 12/30
+- AUDIO / VIDEO IS SYNCHRONIZED PERFECTLY
 THINGS TO CONSIDER FOR CONFIGURABILITY
 - dimensions of videos
 - frame rate of videos
